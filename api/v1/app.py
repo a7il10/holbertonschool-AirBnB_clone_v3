6ff2bb75d57be6dfde17214bdbec9d8503372937
a7il10 <atilio.sulovari199@gmail.com>
2024-04-17 14:03:01 +0200

    update ex 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-import os
-from models import storage
+"""App"""
+
 from api.v1.views import app_views
 from flask import Flask
+from models import storage
+import os
 
-app.register_blueprint(app_views)
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
 @app.teardown_appcontext
-def teardown_db(exception):
+def close_db(error):
+    """Close storage on teardown"""
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""Index"""
+
 from flask import jsonify
 from api.v1.views import app_views
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
+    """Return server status"""
     return jsonify({"status": "OK"})
