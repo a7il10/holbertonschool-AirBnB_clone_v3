4f14085df2488f13c1dad0f05833653005bba6ad
Ferlando <contact@ferlando.me>
2024-04-18 17:22:17 +0200

    add error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,6 +17,12 @@ def close_db(error):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """Return a JSON-formatted 404 status code response"""
+    return {"error": "Not found"}, 404
+
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', '5000')
