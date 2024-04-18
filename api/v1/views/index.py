89b50fba354a7913810e3ce02a37407f6b48cae0
Ferlando <contact@ferlando.me>
2024-04-18 17:09:32 +0200

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,13 +2,21 @@
 """Index module"""
 
 
-from flask import jsonify
+from flask import jsonify, Flask
 from api.v1.views import app_views
 from models import storage
 
 
+app = Flask(__name__)
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def get_status():
+    return jsonify({'status': 'OK'})
+
+
 @app_views.route('/stats', methods=['GET'])
-def get_stats():
+def counter():
     stats = {
         'amenities': storage.count('Amenity'),
         'cities': storage.count('City'),
