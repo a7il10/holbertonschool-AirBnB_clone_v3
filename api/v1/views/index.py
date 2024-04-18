404b48cbb10844d15b337ffea390e79e3c97a712
Ferlando <contact@ferlando.me>
2024-04-18 17:06:30 +0200

    update code

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 """Index module"""
-from api.v1.views import app_views
+
+
 from flask import jsonify
+from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    """Return status"""
-    return jsonify({"status": "OK"})
-
-
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def stats():
-    """Return stats"""
-    classes = {"Amenity": "amenities", "City": "cities",
-               "Place": "places", "Review": "reviews",
-               "State": "states", "User": "users"}
-    new_dict = {}
-    for key, value in classes.items():
-        new_dict[value] = storage.count(key)
-    return jsonify(new_dict)
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
