d511090dcbf6c2f7fc15245ea0fd3a0487c35352
Ferlando <contact@ferlando.me>
2024-04-18 17:02:54 +0200

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
-"""Index"""
-
-from flask import jsonify
+"""Index module"""
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """Return server status"""
+    """Return status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Return stats"""
+    classes = {"Amenity": "amenities", "City": "cities",
+               "Place": "places", "Review": "reviews",
+               "State": "states", "User": "users"}
+    new_dict = {}
+    for key, value in classes.items():
+        new_dict[value] = storage.count(key)
+    return jsonify(new_dict)
